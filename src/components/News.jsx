1fe0459c96740d79d888feab550adc5c650b19e0
Writhney <writhneylm@gmail.com>
2023-02-19 15:50:23 -0500

    update news

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -8,44 +8,50 @@ const News = () => {
         </a>
         <section className="News-container">
             
-            <span className="News-item">
+        <a href="News-link-item">
+            <div className="News-item">
                 <figure className="News-item-container-img">
-                    <a href="News-link-item">
+                    
                         <img src="https://i.postimg.cc/jdVVfFXs/DSC-0433.jpg" alt="imagen" className="News-item-img" />
-                    </a>
+                    
                 </figure>
                 <div className='News-item-container-info mod-position-info-big'>
                     <p className="News-item-date">Publicado 18/01/2023 15:31</p>
                     <h4 className="News-item-title">CUADRO DE MÉRITO DEL PROCESO DE REASIGNACIÓN EXCEPCIONAL DOCENTE 2023 - UGEL ANDAHUAYLAS.</h4>
                 </div>
-            </span>
-            
-            <span className="News-item">
+            </div>
+        </a>  
+        <a href="News-link-item">
+            <div className="News-item">
                 <figure className="News-item-container-img">
-                    <a href="News-link-item">
+                    
                         <img src="https://i.postimg.cc/5N8hSS78/DSC-0628.jpg" alt="imagen" className="News-item-img" />
-                    </a>
+                    
                 </figure>
                 <div className='News-item-container-info'>
                     <p className='News-item-date mod-size-date'>Publicado 18/01/2023 15:31</p>
                     <h4 className='News-item-title mod-size-title'>CUADRO DE MÉRITO DEL PROCESO DE REASIGNACIÓN EXCEPCIONAL DOCENTE 2023 - UGEL ANDAHUAYLAS.</h4>
                 </div>
-            </span>
-
-            <span className="News-item">
+            </div>
+        </a>
+        <a href="News-link-item">
+            <div className="News-item">
                 <figure className="News-item-container-img">
-                    <a href="News-link-item">
+                    
                         <img src="https://i.postimg.cc/sxht8YVd/IMG-2346.jpg" alt="imagen" className="News-item-img" />
-                    </a>                
+                                    
                 </figure>
                 <div className='News-item-container-info'>
                     <p className='News-item-date mod-size-date'>Publicado 18/01/2023 15:31</p>
                     <h4 className='News-item-title mod-size-title'>CUADRO DE MÉRITO DEL PROCESO DE REASIGNACIÓN EXCEPCIONAL DOCENTE 2023 - UGEL ANDAHUAYLAS.</h4>
                 </div>
-            </span>
+            </div>
+        </a>
         </section>
-        <button className='News-button'>Ver mas</button>
-        </div>    
+        <div className='News-button'>
+            <a href="https://www.youtube.com/@ugelandahuaylas8739" target="_blank" className='news-link-button'>Ver mas</a>
+        </div>
+    </div>    
     </>  
     )
     
